0bbda5b722838d8105d6ff4738b959729d60c440
Tingwei0512 <tingwewe@gmail.com>
2021-04-08 23:10:24 +0800

    hw06 version1

---
+++ HW06_4108056038_1.java
@@ -0,0 +1,33 @@
+public class HW06_4108056038_1 extends Dessert_Desert {
+    public int[] maxBlocks(int[][] inputArr) {
+        int len = inputArr.length;
+        int[] re = new int[len];
+        for(int i=0;i<len;i++) {
+            int jlen = inputArr[i].length;
+            int[] max = new int[jlen];
+            int[] min = new int[jlen];
+            int count=1;
+            max[0]=min[0]=inputArr[i][0];
+            for(int j=1;j<jlen;j++) {
+                if(inputArr[i][j]>=max[count-1]) {
+                    count++;
+                    max[count-1]=min[count-1]=inputArr[i][j];
+                }else if (inputArr[i][j]<min[count-1]) {
+                    min[count-1]=inputArr[i][j];
+                    for (int check=count-2;check>=0;check--) {
+                        if (inputArr[i][j]>max[check]) break;
+                        else if (inputArr[i][j]>min[check]) {
+                            count--;
+                            break;
+                        }else {
+                            count--;
+                            min[check]=inputArr[i][j];
+                        }
+                    }
+                }
+            }
+            re[i]=count; 
+        }
+        return re;
+    }
+}

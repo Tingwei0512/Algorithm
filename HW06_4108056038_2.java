87edb62adb1ac4c5c40b008c6525a4bfaf1f0c97
Tingwei0512 <tingwewe@gmail.com>
2021-04-09 21:03:02 +0800

    debug

--- HW06_4108056038_2.java
+++ HW06_4108056038_2.java
@@ -1,7 +1,7 @@
 public class HW06_4108056038_2 extends Dessert_Desert {
     public static void main(String[] args) {
         HW06_4108056038_2 test = new HW06_4108056038_2();
-        int[][] a = {{1,1,1,1,1,1,1},{1,3,5,7,9},{1,2,3},{5,4,3,2,1},{2,1,3,2},{1,4,5,4,9,8},{1,4,8,5,4,9,7,9},{1},{2,1},{7,2,5,8,4,2,8,9},{3,2,1,2,4,6,5,2},{3,2,5,1,9},{1,2,9,9,2}};
+        int[][] a = {{1,4,8,5,4,9,7,9},{1},{3,2,5,1,9},{1,2,9,9,2}};
         for(int i=0;i<a.length;i++) {
             System.out.println(test.maxBlocks(a)[i]);
         }
@@ -22,10 +22,15 @@ public class HW06_4108056038_2 extends Dessert_Desert {
                 }else if (inputArr[i][j]<min[count-1]) {
                     min[count-1]=inputArr[i][j];
                     for (int check=count-2;check>=0;check--) {
-                        if(inputArr[i][j]<min[check]) {
+                        if (inputArr[i][j]>=max[check]) break;
+                        else if (inputArr[i][j]>=min[check]) {
+                            count--;
+                            max[check]=max[check+1];
+                            break;
+                        }else {
                             count--;
                             min[check]=inputArr[i][j];
-                        } else break;
+                        }
                     }
                 }
             }
